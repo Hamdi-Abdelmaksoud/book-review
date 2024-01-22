12f9406346bc33240090a38353e02ebe95473553
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2024-01-22 12:19:28 +0100

    create tables and relation

--- database/migrations/2024_01_22_104955_create_reviews_table.php
+++ database/migrations/2024_01_22_104955_create_reviews_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            // $table->unsignedBigInteger('book_id');//foreign key
+            $table->text('review');
+            $table->unsignedTinyInteger('rating');
+            // $table->foreign('book_id')->references('id')->on('books')
+            // ->onDelete('cascade');//define the foreign key
             $table->timestamps();
+            $table->foreign('book_id')->constrained()->cascadeOnDelete();
         });
     }
 
