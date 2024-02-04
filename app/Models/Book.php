f2ced8dc552f4d610372eeefc849c65f81141acc
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2024-02-04 12:20:51 +0100

    loca query scopes

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -3,7 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Book extends Model
 {
@@ -11,6 +11,22 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+    public function scoopeTitle(Builder $query, string $title):Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
+    }
+
+    public function scopePopular(Builder $query): Builder
+    {
+        return $query->withCount('reviews')
+            ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query): Builder
+    {
+        return $query->withAvg('reviews', 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
     }
 
 }
