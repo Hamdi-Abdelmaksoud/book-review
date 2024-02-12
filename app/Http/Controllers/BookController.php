23e1a1c02216cd2ad2ed57391054739ea34c37fd
Hamdi-Abdelmaksoud <hamdi.wdev@gmail.com>
2024-02-12 16:13:55 +0100

    ajouter des filtres

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -14,8 +14,9 @@ class BookController extends Controller
     {
         $title =$request->input('title');//input méthode reccupére des données post ou get
         $books=Book::when($title,
-        fn($query, $title) => $query->title($title)
-    )->get();
+        fn($query, $title) => $query->title($title)//title fait référence à la fonction défini en local scoopTitle
+    //$query instance du querybuilder
+        )->get();//si le $title est non vide la fonction va s'exucuter
         return view('books.index', ['books' => $books]);
     }
 
